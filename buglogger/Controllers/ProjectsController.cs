730f865a3592d0cca0dcb5edf22cba570592b9d8
garrett_adamss <garrettadams27@gmail.com>
2022-10-11 18:25:08 -0600

    projects outline

--- buglogger/Controllers/ProjectsController.cs
+++ buglogger/Controllers/ProjectsController.cs
@@ -6,5 +6,12 @@ namespace buglogger.Controllers
     public class ProjectsController : ControllerBase
     {
         private readonly ProjectsService _projectsService;
+        private readonly ProjectBugsService _projectsBugsService;
+
+        public ProjectsController(ProjectsService projectsService, ProjectBugsService projectsBugsService)
+        {
+            _projectsService = projectsService;
+            _projectsBugsService = projectsBugsService;
+        }
     }
 }
--- buglogger/Services/projectBugsService.cs
+++ buglogger/Services/projectBugsService.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace buglogger.Repositories
+namespace buglogger.Services
 {
-    public class projectBugsService
+    public class ProjectBugsService
     {
         
     }
