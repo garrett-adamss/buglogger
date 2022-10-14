b44113635064e550f753abe44c23cb49a6c42276
garrett_adamss <garrettadams27@gmail.com>
2022-10-13 22:47:38 -0600

    navbarwork

--- buglogger.client/src/components/Navbar.vue
+++ buglogger.client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" src="https://logoeps.com/wp-content/uploads/2014/04/3886-b-logo-square-icon-vector-icon-vector-eps.png" height="45" />
       </div>
     </router-link>
     <button
@@ -19,9 +19,10 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
+          Bugs
+        </li>
+        <li>
+          Projects
         </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
