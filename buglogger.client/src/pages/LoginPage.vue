518557b069d08581a37762298dd2f5dc82d99823
garrett_adamss <garrettadams27@gmail.com>
2022-10-13 23:06:22 -0600

    login page

---
+++ buglogger.client/src/pages/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>Welcome to bugLogger</h1>
+    <h3>Sign in or create and account to continue</h3>
+</div>
+</template>
+ 
+<script>
+export default {
+   setup(){
+      return {
+ 
+      }
+   }
+}
+</script>
+ 
+<style> 
+
+</style>
