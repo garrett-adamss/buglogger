c25101bc1594ed37f065e1eade5eddbf02f7896c
garrett_adamss <garrettadams27@gmail.com>
2022-10-17 23:20:10 -0600

    Project component

--- buglogger.client/src/components/Project.vue
+++ buglogger.client/src/components/Project.vue
@@ -1,8 +1,8 @@
 <template>
      <div class="project d-flex row rounded mb-1">
-    <div class="col-1 ms-3 selectable"><p class="project-desc">High</p></div>
+    <div class="col-1 ms-3 selectable"><p class="project-desc">pfp</p></div>
     <div class="col-2 offset-1 selectable"><p class="project-desc">Keepr</p></div>
-    <div class="col-2 selectable"><p class="project-desc">router-link</p></div>
+    <div class="col-2 selectable"><p class="project-desc">5</p></div>
     <div class="col-4"><p class="project-desc">user router-link from the vue card to the...</p></div>
     <div class="col-1"><p class="project-desc"><i class="mdi mdi-check-circle selectable"></i></p></div>
   </div>
--- buglogger.client/src/pages/ProjectsPage.vue
+++ buglogger.client/src/pages/ProjectsPage.vue
@@ -1,7 +1,14 @@
 <template>
-    <h1>Project Page</h1>
+    <div class="top-line">
+    </div>
+    <div class="d-flex row align-items-center project-title">
+        <div class="col-1 ms-3"><p class="project-subtitle user-select-none">User</p></div>
+        <div class="col-2 offset-1 selectable" @click="toProject()"><p class="project-subtitle">Project</p></div>
+        <div class="col-2 selectable" @click="toBug()"><p class="project-subtitle">Bug#</p></div>
+        <div class="col-4"><p class="project-subtitle user-select-none">Description</p></div>
+        <div class="col-1"><p class="project-subtitle user-select-none">Status</p></div>
+    </div>
     <Project/>
-
 </template>
  
 <script>
@@ -15,4 +22,18 @@ export default {
 </script>
  
 <style> 
+.top-line{
+  border-top: 2.5px solid rgb(141, 141, 141);
+}
+.project-subtitle{
+  margin-bottom: 12px;
+  margin-top: 9.5px;
+  font-size: 18px;
+  /* margin-left: 5px; */
+  max-width: fit-content;
+}
+.project-title{
+  margin-left: .5px;
+  margin-right: 1px;
+}
 </style>
