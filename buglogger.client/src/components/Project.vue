e07e5fff88bb0341e90ebd15aaa6f630f20d7000
garrett_adamss <garrettadams27@gmail.com>
2022-10-27 23:48:39 -0600

    project

--- buglogger.client/src/components/Project.vue
+++ buglogger.client/src/components/Project.vue
@@ -1,7 +1,7 @@
 <template>
      <div class="project d-flex row rounded mb-1">
     <div class="col-1 ms-3 selectable"><p class="project-desc">pfp</p></div>
-    <div class="col-2 offset-1 selectable"><p class="project-desc">Keepr</p></div>
+    <div class="col-2 offset-1 selectable" @click="toProject()"><p class="project-desc">Keepr</p></div>
     <div class="col-2 selectable"><p class="project-desc">5</p></div>
     <div class="col-4"><p class="project-desc">pintrest style application where users...</p></div>
     <div class="col-1"><p class="project-desc"><i class="mdi mdi-check-circle selectable"></i></p></div>
@@ -20,12 +20,13 @@ export default {
  
 <style> 
 .project{
+    background-color: aqua;
     margin-left: .5px;
     margin-right: 1px;
-    border-top: 2px solid rgb(176, 176, 176);
-    border-bottom: 2px solid rgb(176, 176, 176);
-    border-right: 2px solid rgb(176, 176, 176);
-    border-left: 2px solid rgb(176, 176, 176);
+    border-top: 2px solid rgb(88, 186, 32);
+    border-bottom: 2px solid rgb(33, 153, 200);
+    border-right: 2px solid rgb(33, 153, 200);
+    border-left: 2px solid rgb(33, 153, 200);
 }
 .project-desc{
     margin-top: 0.75em;
