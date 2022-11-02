b90613dad15ebcf16dbddae8e64e0c7b5c1ab5f3
garrett_adamss <garrettadams27@gmail.com>
2022-11-01 20:45:06 -0600

    style change

--- buglogger.client/src/pages/HomePage.vue
+++ buglogger.client/src/pages/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
-HOME PAGE
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-3 selection-box">
+          test
+      </div>
+      <div class="col-3">
+        test
+      </div>
+      <div class="col-3">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +19,7 @@ HOME PAGE
 </script>
 
 <style scoped lang="scss">
+.selection-box{
+
+}
 </style>
