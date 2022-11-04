d09504c0c6d5329b06badca7c52452ff7bfebe58
garrett_adamss <garrettadams27@gmail.com>
2022-11-03 23:48:11 -0600

    title

--- buglogger.client/src/components/NewProjectModal.vue
+++ buglogger.client/src/components/NewProjectModal.vue
@@ -3,7 +3,7 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Create Project</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Close Modal"></button>
       </div>
       <div class="modal-body">
