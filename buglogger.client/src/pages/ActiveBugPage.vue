fd2dbffa13fb37e0588d2d4d4ab42210a9847257
garrett_adamss <garrettadams27@gmail.com>
2022-11-10 22:56:24 -0700

    active bug

--- buglogger.client/src/pages/ActiveBugPage.vue
+++ buglogger.client/src/pages/ActiveBugPage.vue
@@ -1,12 +1,19 @@
 <template>
- <div>Active bug page</div>
+ <div>
+    Active bug page
+    {{ bug.name }}
+</div>
 </template>
  
 <script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
 export default {
    setup(){
       return {
- 
+        bug: computed(() => AppState.activeBug)
+
+        
       }
    }
 }
