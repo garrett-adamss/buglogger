bef0ef5d6aac3b826ef66239e71caf28d539ff91
garrett_adamss <garrettadams27@gmail.com>
2022-11-16 17:49:18 -0700

    left nav bar

--- buglogger.client/src/components/Navbar.vue
+++ buglogger.client/src/components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
-<nav class="flex-column">
-  <div>
+<nav class="row nav">
+  <div >
     <img alt="logo" src="https://logoeps.com/wp-content/uploads/2014/04/3886-b-logo-square-icon-vector-icon-vector-eps.png" height="45" />
   </div>
-  <router-link class="navbar-brand" :to="{ name: 'BugPage' }">
-    Bugs
+  <router-link class="navbar-brand col-8" :to="{ name: 'BugPage' }">
+   <p class="selectable">Bugs</p> 
   </router-link>
-  <router-link class="navbar-brand" :to="{ name: 'BugPage' }">
-    Projects
+  <router-link class="navbar-brand col-8" :to="{ name: 'ProjectsPage' }">
+    <p class="selectable">Projects</p>
   </router-link>
-  <router-link class="navbar-brand" :to="{ name: 'BugPage' }">
-    Bugs
+  <router-link class="navbar-brand col-8" :to="{ name: 'BugPage' }">
+    <p class="selectable">Bugs</p>
   </router-link>
   <Login />
 </nav>
@@ -40,5 +40,13 @@ a:hover {
   border-bottom: 2px solid blueviolet;
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
+}
+
+.
+
+.nav{ 
+  height: 100vh;
+  border-right: 2px solid rgb(0, 0, 0);
+  background-color: rgba(64, 64, 64, 0.459);
 }
 </style>
